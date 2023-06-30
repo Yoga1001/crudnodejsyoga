bccf43458519904810ccdf05f927c4545adf7b78
unknown <myoga196@gmail.com>
2023-06-30 14:49:11 +0700

    first commit

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const PORT = process.env.PORT || 5000;
 
 const multer = require('multer')
 const path = require('path')
-
+const cors = require('cors');
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors ({
+    origin :'*'
+}))
 // script upload
 
 app.use(express.static("./public"))
